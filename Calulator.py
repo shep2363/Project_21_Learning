770f30fa5e0ae86a11712b702feb84eac1c6b4c2
shep2363 <117785624+shep2363@users.noreply.github.com>
2024-07-25 18:52:18 -0600

    Calulator created

---
+++ Calulator.py
@@ -0,0 +1,66 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y ==0:
+        return "Error! Division by zero"
+    else:
+        return x / y
+    
+
+
+def calculator():
+    while True:
+        print('Select operation:')
+        print('1. Add')
+        print('2. Subtract')
+        print('3. Multiply')
+        print('4. Divide')
+        print('5. Exit')
+
+        choice = input('Enter choice: ')
+
+        if choice in ['1', '2', '3', '4']:
+            try:
+                num1 = float(input('Enter first number: '))
+                num2 = float(input('Enter second number: '))
+
+            except ValueError:
+                print('Invalid input! Please enter numeric values')
+                continue
+
+
+        if choice == '1':
+            print(f"The result is: {add(num1, num2)}")
+        elif choice == '2':
+            print(f"The result is: {subtract(num1, num2)}")
+        elif choice == '3':
+            print(f"The result is: {multiply(num1, num2)}")
+        elif choice == '4':
+            print(f"The result is: {divide(num1, num2)}")
+
+
+        elif choice == '5':
+            print('Goodbye!')
+            break        
+
+if __name__ == '__main__':
+    calculator()
+
+
+
+
+
+
+
+
+
+
+
+
